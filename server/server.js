fc3f4b10e06271a2823350b6359fcb9de7684915
Udit Narayan <uditnarayan.gmjc@gmail.com>
2024-12-13 12:40:06 +0530

    url rechanged

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "https://mern-ecommerce-1-9zff.onrender.com",
+    origin: process.env.CLIENT_BASE_URL,
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
